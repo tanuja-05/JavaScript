0a9bf2c50f9e45ca1374948378dd667c081aa4b8
tanuja-05 <131127442+tanuja-05@users.noreply.github.com>
2024-06-07 08:50:38 +0000

    conversions

---
+++ Basics/03_ConversionsOperations.js
@@ -0,0 +1,20 @@
+// DATA TYPE CONVERSIONS
+
+let val=3 // to convert in string datatype : val=String(3)
+let val1=Number("333aa") 
+
+let str=String(val)
+console.log(typeof str);
+// console.log(typeof val); --> number
+// console.log(typeof(val1)); --> number
+// console.log(val1) --> NaN
+
+//<--observasion-->
+
+// "65" => string
+// "65tm" => NaN
+// true => 1 & false = 0
+
+//type of 1 = true | 0 = false
+// type of "" = false
+// type of "tanuja" = true
