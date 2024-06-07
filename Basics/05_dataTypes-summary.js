3adc46c68d0cf202170cc2c0241f252ee2cec222
tanuja-05 <131127442+tanuja-05@users.noreply.github.com>
2024-06-07 10:26:48 +0000

    dataTypes-summary

---
+++ Basics/05_dataTypes-summary.js
@@ -0,0 +1,5 @@
+/* Data types are divided in two types Primitives and Non Primitives 
+--> they are generally categorized into How data is stored in the memory and How data is accessed from the memory
+
+
+*/
