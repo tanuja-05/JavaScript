2550e7abe3d07dc4233c9412000e15432eaed02f
tanuja-05 <131127442+tanuja-05@users.noreply.github.com>
2024-07-10 16:05:59 +0530

    02_dataTypes.js

--- Basics/02_dataTypes.js
+++ Basics/02_dataTypes.js
@@ -1,9 +1,9 @@
-//since js is has newer versions and to treat all syntaxes & code as same
+//since js  has newer versions and to treat all syntaxes & code as same
 // we use "use strict" to treat all js code as newer version
 
 "use strict";
 
-// alert("hello") => we are in node environment ,js engine is in browser so it get's executed in browser
+// alert("hello") => We are in node environment, js engine is in the browser so it gets executed in browser
 
 //the code written should be readable
 
@@ -14,11 +14,11 @@
 //bigint =>for larger values
 //string =>written in double quotations("")
 //boolean => true/false
-//null =>standalone value ( ex: no temperature is null ,but 0 temprature is considered to be a temperature)
+//null =>standalone value ( ex: no temperature is null, but 0 temperature is considered to be a temperature)
 //undefined => declared but not initialized
-//symbol => uniquness widely used in reactjs
+//symbol => uniqueness widely used in reactjs
 
-// NOTE : null is object type but undefined is type undefined itself
+// NOTE: null is object type but undefined is type undefined itself
 
 let a="hello js series"
 let b=6
